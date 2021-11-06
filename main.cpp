396d21bad0521fcd1b50663c7ff62fb297963a51
ellenyim8 <eyim009@ucr.edu>
2021-11-06 13:28:19 -0700

    added ExcelColumnNumber function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 using namespace std;
 
+int getColumnNumber(string columnString);
+
 int main() 
 {
-    cout << "testing..." << endl; 
+    //cout << "testing..." << endl; 
+    cout << getColumnNumber("ABC") << endl;
+    cout << "Expected: 731." << endl; 
+
+    cout << getColumnNumber("DEF") << endl; 
+    cout << "Expected: 2840. " << endl; 
+
+    cout << getColumnNumber("AA") << endl;
+    cout << "Expected: 27." << endl; 
+
+    cout << getColumnNumber("CC") << endl; 
+    cout << "Expected: 81." << endl; 
+
+    cout << getColumnNumber("XFE") << endl; 
+    cout << "Expected: 16385." << endl; 
+
     return 0;
 }
 
+int getColumnNumber(string columnString)
+{
+    int columnNumber = 0;
+
+    for (auto ch : columnString) {
+        columnNumber = columnNumber * 26 + (ch - 'A' + 1);
+    }
+
+    return columnNumber;
+}
 
